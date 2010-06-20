37012e858a59f79711b3fb4589ac5f07e08ea424
Andrey Davydov <andrey.a.davydov@gmail.com>
2010-06-21 00:49:16 +0400

    viewer disabled, console out extracted watermark.

--- src/main.cpp
+++ src/main.cpp
@@ -158,8 +158,62 @@ int main( int argc, char** argv )
     size_t max_subarea_size = params["max-subarea-size"].as< size_t >();
     embedded_watermark_t ew = watermarking::embed( graph, max_subarea_size, weighted, use_edges, params["step-by-step"].as< bool >() );
                                                              
-    typedef my_visualizer< embedded_watermark_t::element_type > visualizer_t; 
-    visualizer_t v( ew.get() ); 
-    boost::format title("W: weighted = %1%, use-edges = %2%");
-    visualization::viewer< visualizer_t > viewer( &v, ( title % weighted % use_edges ).str().c_str(), &argc, argv );
+    namespace po = boost::program_options;
+
+    std::ifstream conf("embedding.conf");
+                
+    po::options_description desc;
+    desc.add_options()
+        ( "message", po::value< std::string >() )
+        ( "key",     po::value< int >() )
+        ( "chip-rate", po::value< size_t >() )
+        ( "alpha",   po::value< double >() )
+    ;
+
+    po::variables_map vm;       
+    po::store( po::parse_config_file( conf, desc ), vm );
+
+    std::string message_text = vm["message"].as< std::string >();
+    std::cout << message_text << std::endl;
+    
+    watermarking::message_t message( message_text.size() * 7 );
+    for ( size_t i = 0; i != message_text.size(); ++i )
+    {
+        unsigned char c = message_text[i];
+        for ( size_t j = 0; j != 7; ++j )
+        {
+            message[i * 7 + j] = c % 2;
+            c /= 2;
+        }
+    }
+    
+    ew->modify_vertices( message,   vm["chip-rate"].as< size_t >(),
+                                    vm["key"].as< int >(),
+                                    vm["alpha"].as< double >() );
+    graph_t modified_graph = ew->modified_graph();
+    for ( double radius = 0.1; radius <= 1.0; radius += 0.1 )
+    {
+        graph_t noised_graph = watermarking::add_noise( modified_graph, radius );
+        watermarking::message_t message = watermarking::extract( noised_graph, 
+                                                                 max_subarea_size,
+                                                                 weighted )->extract( vm["chip_rate"].as< size_t >(), vm["key"].as< int >(), ew->coefficients_, message_text.size() ); 
+        std::cout << "Extracted message: ";
+        for ( size_t i = 0; i != message.size(); )
+        {
+            unsigned char c = 0;
+            int pow = 1;
+            for ( size_t j = 0; j != 7; ++j, ++i, pow *= 2 )
+            {
+                c += message[i] * pow;
+            }
+            std::cout << c;
+        }
+        std::cout << std::endl;
+    }
+
+ 
+//    typedef my_visualizer< embedded_watermark_t::element_type > visualizer_t; 
+//    visualizer_t v( ew.get() ); 
+//    boost::format title("W: weighted = %1%, use-edges = %2%");
+//    visualization::viewer< visualizer_t > viewer( &v, ( title % weighted % use_edges ).str().c_str(), &argc, argv );
 }
