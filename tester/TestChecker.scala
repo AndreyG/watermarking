0f2d130c9973a240269fe8d1c210fc312a10ea24
Andrey Davydov <andrey.a.davydov@gmail.com>
2010-10-16 23:12:00 +0400

    +1

--- tester/TestChecker.scala
+++ tester/TestChecker.scala
@@ -2,6 +2,8 @@ import java.io.{File, PrintStream, FileOutputStream}
 import scala.io.Source
 import java.lang.Double.parseDouble
 import Common._
+import scala.collection.Map
+import scala.collection.immutable.{TreeMap}
 
 object TestChecker {
   def main(args: Array[String]) {
@@ -9,26 +11,29 @@ object TestChecker {
     for (graphDir <- dirs(inputDir, matchPattern(args(0)))) {
       for (factorizationDir <- dirs(graphDir)) {
 	for (embeddingDir <- dirs(factorizationDir, nameStarts("alpha"))) {
+	  var attempts: Map[Double, (Int, Int)] = new TreeMap
 	  for (timeDir <- dirs(embeddingDir)) {
 	    for (noiseDir <- dirs(timeDir, nameStarts("noise"))) {
-	      val noise = {
-		val x = parseDouble(noiseDir.getName.substring(6))
-		format("%f", x)
-	      }			    
-	      val name = factorizationDir.getName + "/" + graphDir.getName + "/" + embeddingDir.getName + "/noise-" + noise
-	      var attemptsNum = 0
-	      var successesNum = 0
+	      val noise = parseDouble(noiseDir.getName.substring(6))
+	      var (a, s) = if (attempts contains noise) attempts(noise) else (0, 0)
 	      for (attemptDir <- dirs(noiseDir, nameStarts("attempt"))) {
 		val f = new File(attemptDir, "message.txt")
 		if (f.exists) {
-		  attemptsNum += 1
+		  a += 1 
 		  val lines = Source.fromFile(f).getLines.toArray
 		  if ((lines.length == 4) && (lines(1) == lines(3)))
-		    successesNum += 1
+		    s += 1
 		}
 	      }
-	      println(format("%s: (%d/%d)", name, successesNum, attemptsNum))
+	      if (attempts contains noise)
+		attempts -= noise
+	      val newElem = (noise, (a, s))
+	      attempts += newElem
 	    }
+	  }
+	  for ((noise, (attemptsNum, successesNum)) <- attempts) { 
+	    val name = factorizationDir.getName + "/" + graphDir.getName + "/" + embeddingDir.getName + "/noise-" + "%f".format(noise)
+	    println(format("%s: (%d/%d)", name, successesNum, attemptsNum))
 	  }
 	}
       }
